571e043721e5a1283db5f6f44ecbc5498573a7c2
Nhi Quach <nhiquach@berkeley.edu>
2016-03-09 00:13:13 -0800

    part 1 required functionality complete

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -10,15 +10,111 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+
+    @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var incorrectGuessesLabel: UILabel!
+    @IBOutlet weak var hangmanStateImageView: UIImageView!
+    @IBOutlet weak var phraseLabel: UILabel!
+    
+    var phrase: String = ""
+    var correctGuesses = Set<String>()
+    var incorrectGuesses = Set<String>()
+    var hangmanState = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        phrase = hangmanPhrases.getRandomPhrase()
+        
+        setBlanks()
+        incorrectGuessesLabel.text = ""
+        
+        loadTargets()
+        
         print(phrase)
     }
-
+    
+    func setBlanks() {
+        var blanks = ""
+        for character in phrase.characters {
+            if character != " " {
+                blanks += "_ "
+            } else {
+                blanks += " "
+            }
+        }
+        
+        phraseLabel.text = blanks
+    }
+    
+    
+    func inputGuess() {
+        if let letter = guessTextField.text {
+            if letter.characters.count > 1 {
+                return
+            }
+            if !phrase.uppercaseString.containsString(letter.uppercaseString) {
+                updateIncorrectGuesses(letter.uppercaseString)
+            } else {
+                updateBlanks(letter.uppercaseString)
+            }
+        }
+        
+    }
+    
+    func updateIncorrectGuesses(letter: String) {
+        if !incorrectGuesses.contains(letter) {
+            updateHangman()
+        }
+        
+        incorrectGuesses.insert(letter)
+        var incorrect = ""
+        for letter in incorrectGuesses {
+            incorrect += letter + " "
+        }
+        incorrectGuessesLabel.text = incorrect
+        
+    }
+    
+    func updateHangman() {
+        if hangmanState >= 7 {
+            return
+        }
+        hangmanState += 1
+        let imageFileName = "hangman" + String(hangmanState) + ".gif"
+        hangmanStateImageView.image = UIImage(named: imageFileName)
+    }
+    
+    func updateBlanks(letter: String) {
+        correctGuesses.insert(letter)
+        
+        var blanks = ""
+        for character in phrase.characters {
+            var blank = true
+            for guess in correctGuesses {
+                if guess[guess.startIndex] == character {
+                    blank = false
+                    blanks += guess + " "
+                }
+            }
+            if blank == true && character != " "{
+                blanks += "_ "
+            }
+            if character == " " {
+                blanks += " "
+            }
+        }
+        phraseLabel.text = blanks
+    }
+    
+    func loadTargets() {
+        guessButton.addTarget(self, action: "inputGuess", forControlEvents: .TouchUpInside)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
